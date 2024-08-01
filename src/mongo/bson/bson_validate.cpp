d63fd94c41ca7b43a57ace74eb81a213450cb810
TL-Yao <tongle@scntist.com>
2024-08-01 11:42:23 +0000

    commit patch 1978304

--- src/mongo/bson/bson_validate.cpp
+++ src/mongo/bson/bson_validate.cpp
@@ -78,6 +78,11 @@ namespace mongo {
                     return makeError("invalid bson", _idElem);
                 }
 
+                if ( sz <= 0 ) {
+                    // must have NULL at the very least
+                    return makeError("invalid bson", _idElem);
+                }
+
                 if ( out ) {
                     *out = StringData( _buffer + _position, sz );
                 }
